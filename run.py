85fbe3a271b67779619a7b2fd068279af02e562b
gyusu han <slhopy@gmail.com>
2018-02-14 20:17:26 +0900

    update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import h5py
 import json
-import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 import etl, lstm, plot, stats
@@ -18,38 +18,53 @@ dl = etl.ETL(
     y_col=configs['data']['y_predict_column'],
     filter_cols=configs['data']['filter_columns'],
     target_percent_change=configs['rule']['target_percent_change'],
+    losscut_percent_change=configs['rule']['losscut_percent_change'],
     train_test_split=configs['data']['train_test_split']
 )
-
-# dl.create_clean_datafile()
+if input("DO YOU WANT TO CREATE NEW DATAFILE ? (y / n)") in ['y','Y']:
+    dl.create_clean_datafile()
 
 with h5py.File(configs['data']['filename_clean'], 'r') as hf:
     nrows = hf['x'].shape[0]
     ncols = hf['x'].shape[2]
+    ntrain = int(hf['ntrain'][0])
 
-    print(hf['x'].shape)
-    print(hf['y'].shape)
+    # check the num of each class
+    num_minority = sum(hf['y'][:ntrain])
+    num_majority = ntrain - num_minority
 
-ntrain = int(configs['data']['train_test_split'] * nrows)
+    # calculate the imbalance ratio. used for construct the loss(cost) fn.
+    imbalance_ratio = num_majority / num_minority
+
 steps_per_epoch = int(ntrain / configs['data']['batch_size'])
 
-# ntrain를 batch_size의 배수로 만들어 경계 값을 명확히 한다.
-ntrain = steps_per_epoch * configs['data']['batch_size']
 print('> Clean data has', nrows, 'data rows. Training on', ntrain, 'rows with', steps_per_epoch, 'steps-per-epoch')
+print('> Class 0: {}, Class 1: {} --in training set'.format(num_majority, num_minority))
+print('> imbalance_ratio(class_weight): {}'.format(imbalance_ratio))
 
 # Building a model
 sess =tf.Session()
 model = lstm.LSTM(sess, configs['data']['x_window_size'], ncols,
-                     configs['model']['dirname_save_model'])
+                     configs['model']['dirname_save_model'], class_weight=imbalance_ratio)
 
 sess.run(tf.global_variables_initializer())
 
-# Train the model
-# data_gen_train = dl.generate_clean_data(0, ntrain)
-# model.training(configs['model']['epochs'], steps_per_epoch, data_gen_train, save=True)
 
-# Load a trained model
-model.load_model('epoch0_loss6.74e-01')
+if configs['model']['filename_load_model']:
+    ans = input('DO YOU WANT TO LOAD TRAINED MODEL ? \'{}\' (y / n)'.format(configs['model']['filename_load_model']))
+
+    if ans in['y', 'Y']:
+        # Load a trained model
+        model.load_model(configs['model']['filename_load_model'])
+    else:
+        # Train the model
+        data_gen_train = dl.generate_clean_data(0, ntrain)
+        model.training(configs['model']['epochs'], steps_per_epoch, data_gen_train, save=True)
+else:
+    # Train the model
+    data_gen_train = dl.generate_clean_data(0, ntrain)
+    model.training(configs['model']['epochs'], steps_per_epoch, data_gen_train, save=True)
+
 
 ntest = nrows - ntrain
 steps_test = int(ntest / configs['data']['batch_size'])
@@ -61,16 +76,23 @@ true_values = []
 predictions = model.predict(steps_test, data_gen_test, true_values)
 
 # Print stats
-s1 = stats.STATS(true_values[-ntest:], predictions[-ntest:], threshold=0.5)
+s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.5)
 s1.print_stats()
-s1 = stats.STATS(true_values[-ntest:], predictions[-ntest:], threshold=0.6)
+s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.6)
 s1.print_stats()
-s1 = stats.STATS(true_values[-ntest:], predictions[-ntest:], threshold=0.7)
+s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.7)
 s1.print_stats()
-s1 = stats.STATS(true_values[-ntest:], predictions[-ntest:], threshold=0.75)
+s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.8)
 s1.print_stats()
-# s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.8)
-# s1.print_stats()
+s1 = stats.STATS(true_values[-ntrain:], predictions[-ntrain:], threshold=0.9)
+s1.print_stats()
 
-# plot the result
+# Plot 1. just plot the results
 plot.plot_results(true_values[-ntest:], predictions[-ntest:])
+
+# read price data
+data = pd.read_csv(configs['data']['filename'], index_col=0)
+true_price_data = data['close'].values[-(ntest + configs['data']['y_window_size']):]
+
+# Plot 2. plot the results with price data(close price)
+plot.plot_results_with_price(true_values[-ntest:], predictions[-ntest:], true_price_data, threshold=0.8)
